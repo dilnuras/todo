f39a6f381d16a2a0bd9ab2776b8d9fdeda6eccf3
dilnuras <77263709+dilnuras@users.noreply.github.com>
2021-01-19 14:02:55 +0600

    added todo_list in test page

--- todo/urls.py
+++ todo/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name = "home"),
-    path("test", test, name = "test"),
+    path("test/", test, name = "test"),
     path("test2/", second)
 ]   + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
